02764ad0535e4c90e4c00fd2ee7ee2c2ebdcd331
luochenhao <3453851623@qq.com>
2023-05-03 10:53:53 +0800

    fix: cmake file reconfig

---
+++ client/src/temp.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: temp.cpp
+    > Author: luochenhao
+    > Mail: 3453851623@qq.com
+    > Created Time: Wed 03 May 2023 10:27:18 CST
+************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello client" << endl;
+    return 0;
+}
